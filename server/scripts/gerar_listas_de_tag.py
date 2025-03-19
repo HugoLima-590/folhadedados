7c6cac5f8b72baab70f361fe37b057b8ad662d9b
GT-HugoLima <hugo.lima@grupotechnik.com.br>
2025-03-19 11:45:18 -0300

    process de todos os templates

--- server/scripts/gerar_listas_de_tag.py
+++ server/scripts/gerar_listas_de_tag.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
 def processar_excel(caminho_arquivo, tag_instrumento):
-    df = pd.read_excel(caminho_arquivo, sheet_name="Projeto BIR", engine="openpyxl")
+    
+    #Lê o arquivo mas só as abas
+    abas = pd.ExcelFile(caminho_arquivo, engine="openpyxl").sheet_names
+    
+    #retorna somente a primeira aba diferente de capa
+    sheet_name = [aba for aba in abas if aba.lower() !="capa"][0]
+    
+    #aciona a aba para a leitura do arquivo
+    df = pd.read_excel(caminho_arquivo, sheet_name= sheet_name, engine="openpyxl")
 
+    #converte em string, substitui quebras de linha por espaço e remove espaços extras no inicio e fim
     df.columns = df.columns.astype(str).str.replace(r"\n", " ").str.strip()
+    
+    #Preenche os valores NaN com os valores de antes
     df = df.ffill()
     
     colunas_interesse = [
@@ -12,8 +23,17 @@ def processar_excel(caminho_arquivo, tag_instrumento):
         df.columns[84], df.columns[88], df.columns[92], df.columns[96],
         df.columns[100], df.columns[104], df.columns[108], df.columns[111], df.columns[121]
     ]
-
+    
     df_filtrado = df[df[df.columns[3]].fillna("").astype(str).str.lower().str.startswith(tag_instrumento.lower())]
+    """
+    Explicação da linha de código acima:
+    Seleciona a 4° coluna,
+    preenche NaN por espaços,
+    converte todos os valores para string e minusculo,
+    filtra as linhas onde o valor da coluna começa com o valor de tag_instrumento,
+    mantém apenas as linhas que anteeram ao critério acima
+    """
+    #Filtra apenas as colunas listadas
     df_filtrado = df_filtrado[colunas_interesse]
 
     df_filtrado.columns = [
---
+++ server/scripts/process_bombas.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from openpyxl import load_workbook
+from datetime import datetime
+
+def exportar_fd(file_path):
+    caminho_template = r"server/excel/templates/Bombas.xlsm"
+    caminho_tags = r"server/excel/tags_filtradas.xlsx"
+
+    wb_template = load_workbook(caminho_template, keep_vba=True)
+    sheet_folhas = wb_template["Folhas"]
+    df_tags = pd.read_excel(caminho_tags)
+
+    mapeamento = {
+        "G7": "Nº Instrumento", "B8": "Fluxograma", "B10": "Tipo",
+        "B12": "Diâmetro", "N25": "Fluído", "N27": "Pressão Oper.",
+        "N29": "Viscosidade", "N31": "Vazão max", "N32": "Vazão min",
+        "N34": "Temperatura oper. max", "N36": "Densidade", "A43": "Nota",
+    }
+
+    data_atual = datetime.today().strftime("%d-%m-%Y")  # Pega a data de hoje no formato desejado
+
+    for _, row in df_tags.iterrows():
+        new_sheet = wb_template.copy_worksheet(sheet_folhas)  
+        nome_aba = str(row["Nº Instrumento"])  
+        new_sheet.title = nome_aba
+
+        for cell, coluna in mapeamento.items():
+            if coluna in row and pd.notna(row[coluna]):
+                new_sheet[cell] = str(row[coluna])
+
+        notas = str(row.get("Nota", "")).split("Nota")
+        for i, nota in enumerate(notas):
+            new_sheet[f"A{43 + i}"] = nota.strip()
+
+    # Pegando apenas a parte inicial da tag (as letras antes de números)
+    tag = str(row["Nº Instrumento"])
+    tag_abreviada = "".join([char for char in tag if char.isalpha()])  # Mantém apenas letras
+
+    # Criando o nome do arquivo no formato desejado
+    caminho_saida = f"server/excel/tag_{tag_abreviada}_fd_preenchido_{data_atual}.xlsm"
+
+    # Salvar o arquivo preenchido uma única vez
+    wb_template.save(caminho_saida)
+    print(f"Arquivo {caminho_saida} gerado com sucesso!")
+
