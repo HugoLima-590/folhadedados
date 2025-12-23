c379db1cdba8d59305f104f848b6da6b05bf0173
GT-HugoLima <112393744+HugoLima-590@users.noreply.github.com>
2025-12-23 17:19:54 -0300

    imagem removida para case tecnico

--- automacao_fd_ld/src/App.jsx
+++ automacao_fd_ld/src/App.jsx
@@ -3,11 +3,6 @@ import TelaInputExcel from "./components/TelaInputExcel";
 export default function App() {
   return (
     <div className="bg-slate-600 w-full h-screen flex flex-col items-center justify-center">
-      <img
-        src="/Texto_do_seu_parágrafo_20250324_114428_0000_page-0001-removebg-preview.png"
-        alt="png"
-        className="bg-gray-100 rounded-2xl w-80 h-25 object-cover object-center mb-16 -mt-1"
-      />
       <TelaInputExcel />
     </div>
   );
